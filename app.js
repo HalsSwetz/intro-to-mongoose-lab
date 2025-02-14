bc1c5b83a3c2277eccce1fe209fd18e23a65201e
Halsey Svetz <halseycheese@gmail.com>
2025-02-13 17:49:50 -0700

    solution to the 3 actions in the prompt, need to revisit for update,delete

---
+++ app.js
@@ -0,0 +1,74 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const prompt = require('prompt-sync')();
+const Customer = require('./models/customer.js');
+
+
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+
+    await runMenuOptions();
+    await mongoose.disconnect();
+    process.exit();
+};
+
+const runMenuOptions = async () => {
+    const menu = `
+    Welcome to the CRM
+    
+    What would you like to do?
+    
+    1. Create a customer
+    2. View all customers
+    3. Update a customer
+    4. Delete a customer
+    5. Quit
+    `;
+    let exit = false;
+    while (!exit) {
+        console.log(menu);
+        const choose = prompt('Number of Action to run: ');
+        const actions = {
+            '1': createCustomer,
+            '2': viewCustomers,
+            // '3': updateCustomer,
+            // '4': deleteCustomer,
+            '5': () => {
+                exit = true;
+                console.log("You have exited the premises");
+            },
+        };
+        if (actions[choose]) {
+            await actions[choose]();
+        } else {
+            console.log('Not a valid choice.');
+        };
+    }
+};
+
+
+const createCustomer = async () => {
+    
+    const name = prompt('Enter name:');
+    const age = prompt('Enter age:');
+    
+    const newCustomer = new Customer({
+        name, age: Number(age),
+    });
+    await newCustomer.save();
+    console.log('Customer created:', newCustomer);
+    };
+
+const viewCustomers = async () => {
+
+    const customers = await Customer.find();
+    customers.forEach(customer => {
+        console.log(`id: ${customer.id} -- Name: ${customer.name}, Age: ${customer.age}`);
+    })
+}
+    
+
+
+connect();
